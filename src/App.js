623fab422b080f761d0cbb65f6eaf9f5b420783e
Pedro Henrique Lomba <p.henrique.lomba@gmail.com>
2023-11-24 08:25:48 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Banner/>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
